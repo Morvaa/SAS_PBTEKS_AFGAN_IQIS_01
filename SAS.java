1b422c43f811c43f958a21a0020dcfa46b2d4d3b
unknown <afganevan@gmail.com>
2024-12-02 21:14:39 +0800

    sas pbtgm

---
+++ SAS.java
@@ -0,0 +1,30 @@
+import java.util.Scanner; //untuk mengimpor sebuah kelas yang bernama Scanner dari paket java.util.
+
+public class SAS {
+    public static void main(String[] args){
+
+        
+                // int angka = 8; 
+        
+               
+                // if (angka % 2 == 0) {
+                //     System.out.println("Hasilnya  genap");
+                // } else {
+                //     System.out.println("Hasilnya  ganjil");
+                // }
+          
+
+//Disebuah hutan yang lebat, terdapat seorang penjaga hutan bernama OWO. OWO memiliki tugas penting untuk menjaga keseimbangan ekosistem hutan. Suatu hari, OWO menemukan bahwa banyak hewan dihutan tersebut sedang mengalami masalah. Mereka tidak bisa menentukan apakah jumlah makanan yang tersebut terkumpul cukup atau nda. OWO memutuskan untuk membantu hewan-hewan tersebut. ia ingin membuat sebuah program yang dapat membantu hewan-hewan itu menentukan apakah jumlah makanan yang mereka kumpulkan adalah ganjil atau genap. Jika jumlah makanan genap, mereka bisa membagi makanan tersebut dengan adil. Namun, jika jumlah makanan ganjil, mereka harus mencari satu makanan lagi agar bisa bagi rata.
+int angka = 8; 
+        
+               
+                if (angka % 2 == 0) {
+                    System.out.println("Jumlah makanan, " + angka + " adalah genap makanan bisa dibagikan merata");
+                } else {
+                    System.out.println("Jumlah makanan, " + angka + " adalah ganjil pergi cari satu makanan lagi agar bisa dibagi rata");
+                }
+          
+
+
+}
+}
